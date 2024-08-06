073feecce5a5abecad8dbe07166fbb0eba654683
Aleksey Shepelev <aleksey.shepelev@scompler.com>
2024-08-06 12:06:25 +0400

    Initial Commit

---
+++ PhotoEditorApp/PhotoEditorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorAppApp.swift
+//  PhotoEditorApp
+//
+//  Created by Aleksey Shepelev on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
